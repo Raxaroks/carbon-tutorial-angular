d51f4df44826a1fb8c341abd3fa607b5287cd064
Jose Andres Ornelas Ayala <andres.ornelas@ibm.com>
2021-05-31 01:26:30 -0500

    Change branch

---
+++ carbon-angular-tutorial/src/app/info/info.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { InfoCardComponent } from "./info-card/info-card.component";
+import { InfoSectionComponent } from "./info-section/info-section.component";
+
+@NgModule({
+    declarations: [
+        InfoCardComponent,
+        InfoSectionComponent
+    ],
+    imports: [CommonModule],
+})
+export class InfoModule {}
--- carbon-angular-tutorial/src/app/repositories/repositories.module.ts
+++ carbon-angular-tutorial/src/app/repositories/repositories.module.ts
@@ -1,28 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-<<<<<<< HEAD:src/app/repositories/repositories.module.ts
-import { RepositoriesRoutingModule } from './repositories-routing.module';
-import { RepoPageComponent } from './repo-page/repo-page.component';
-import {
-	GridModule,
-	TableModule,
-	LinkModule,
-	PaginationModule
-} from 'carbon-components-angular';
-import { RepoTableComponent } from './repo-table/repo-table.component';
-
-@NgModule({
-	declarations: [RepoPageComponent, RepoTableComponent],
-	imports: [
-		CommonModule,
-		RepositoriesRoutingModule,
-		GridModule,
-		TableModule,
-		LinkModule,
-		PaginationModule
-	]
-=======
 import { RepositoriesRoutingModule } from "./repositories-routing.module";
 import { RepoPageComponent } from "./repo-page/repo-page.component";
 import { GridModule, TableModule, LinkModule, PaginationModule } from "carbon-components-angular";
@@ -38,6 +16,5 @@ import { RepoTableComponent } from "./repo-table/repo-table.component";
         LinkModule, 
         PaginationModule
     ]
->>>>>>> 063b0a81d77ee5eb6ab3017e08e33d5a9f2cee53:carbon-angular-tutorial/src/app/repositories/repositories.module.ts
 })
 export class RepositoriesModule { }
